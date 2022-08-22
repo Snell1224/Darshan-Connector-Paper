df6bbfc1ca6823556b97fb84f28c230d515e4a55
Ana <analuisavs10@gmail.com>
2022-08-22 12:39:38 -0400

    new code for logs comparison

--- analysis/darshan_logs.R
+++ analysis/darshan_logs.R
@@ -67,29 +67,29 @@ my_theme <- function() {
 ###########################################################################
 # Write all MPIIO CSV to same file
 
-collec = c(256511, 256512, 256513, 256514, 256515, 
-    256522, 256523, 256524, 256525, 256534)
-nfs = c(256535, 256536, 256537, 256538, 256539,
-    256522, 256523, 256524, 256525, 256534)
+# collec = c(256511, 256512, 256513, 256514, 256515, 
+#     256522, 256523, 256524, 256525, 256534)
+# nfs = c(256535, 256536, 256537, 256538, 256539,
+#     256522, 256523, 256524, 256525, 256534)
 
-read_logs <- function(FILE){
-    read_csv(FILE, col_types=cols()) 
-}
+# read_logs <- function(FILE){
+#     read_csv(FILE, col_types=cols()) 
+# }
 
-csv_files <- list.files(path = "./logs_csv_output/mpi-io/csv", pattern = "*.csv$", recursive = TRUE, all.files = TRUE, full.names = TRUE)
-csv_files
-if(length(csv_files) != 0) {    
-        lapply(csv_files, read_logs) %>%
-        bind_rows -> df.mpi
-}
+# csv_files <- list.files(path = "./logs_csv_output/mpi-io/csv", pattern = "*.csv$", recursive = TRUE, all.files = TRUE, full.names = TRUE)
+# csv_files
+# if(length(csv_files) != 0) {    
+#         lapply(csv_files, read_logs) %>%
+#         bind_rows -> df.mpi
+# }
 
-df.mpi %>%
-    mutate(collective = ifelse((job_id %in% collec), "collective", "nocollective")) %>%
-    mutate(filesystem = ifelse((job_id %in% nfs), "nfs", "lustre")) %>%
-    # Removing this unfinished job 
-    filter(job_id != 256526) -> df
+# df.mpi %>%
+#     mutate(collective = ifelse((job_id %in% collec), "collective", "nocollective")) %>%
+#     mutate(filesystem = ifelse((job_id %in% nfs), "nfs", "lustre")) %>%
+#     # Removing this unfinished job 
+#     filter(job_id != 256526) -> df
 
-write_csv(df, "./logs_csv_output/mpi-io/all_darshan_ldms_mpi.csv")
+# write_csv(df, "./logs_csv_output/mpi-io/all_darshan_ldms_mpi.csv")
 
 ###########################################################################
 # READ INFO ABOUT HACC IO
@@ -412,3 +412,352 @@ write_csv(df, "./logs_csv_output/mpi-io/all_darshan_ldms_mpi.csv")
 #     plot = p,
 #     height=7,
 #     width=7)
+
+#######################################3
+# Comparting Darshan profiler with manual data processing
+# of data collected by LDMS
+
+###########################################################################
+# Read HACC 
+df.hacc <- read_csv("./logs_csv_output/hacc-io/all_darshan_ldms_hacc.csv")
+
+# Execution time of each job
+# df.hacc %>% 
+#     group_by(job_id, particles, `#module`, filesystem) %>%
+#     summarize(n=n(), 
+#         mintime=min(`seg:timestamp` - `seg:dur`), 
+#         maxtime=max(`seg:timestamp`)) %>%
+#     mutate(total_dur = maxtime - mintime) %>%
+#     select(filesystem, particles, job_id, `#module`, total_dur) %>%
+#     arrange(job_id) %>%
+#     as.data.frame()
+
+# Operations captured by Collector
+# df.hacc %>% 
+#     distinct(op)
+
+# Operations per rank
+# df.hacc %>% 
+#     filter(op %in% c("read", "write")) %>%
+#     group_by(job_id, particles, `#module`, filesystem, rank, op) %>%
+#     summarize(n=n()) %>%
+#     arrange(job_id, -n) %>%
+#     filter(n!=9) %>%
+#     as.data.frame()
+# All have the same number for POSIX operations, for STDIO no, 
+# but Darshan does not report STDIO info
+
+# Fastest/Slowest for POSIX op - the intuitive approach
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     group_by(job_id, rank, `#module`) %>%
+#     summarize(n=n(), 
+#         mintime=min(`seg:timestamp` - `seg:dur`), 
+#         maxtime=max(`seg:timestamp`)) %>%
+#     mutate(total_dur = maxtime - mintime) %>%
+#     arrange(job_id, maxtime) %>%
+#     select(job_id, rank, total_dur) %>%
+#     as.data.frame()
+
+# Fastest/Slowest for POSIX op - cumulative read, write, and meta times
+# The metadata we have are times with opens, but even considering it 
+# still shows different results
+# df.hacc %>% 
+#     filter(type == "MET") %>%
+#     filter(job_id == "256542") %>%
+#     distinct(`#module`) 
+
+# df.hacc %>% 
+#     filter(type == "MET") %>%
+#     filter(job_id == "256542") %>%
+#     distinct(op) 
+
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     filter(op %in% c("read", "write")) %>%
+#     group_by(job_id, rank) %>%
+#     summarize(n=n(), 
+#         u_ops = toString(unique(op)),
+#         distinct_operations = paste(u_ops, collapse = ', '),
+#         total_dur = sum(`seg:dur`),
+#         total_bytes = sum(`seg:len`)) %>%
+#     arrange(job_id, total_dur) %>%
+#     select(job_id, rank, distinct_operations, total_dur, total_bytes) %>%
+#     as.data.frame()
+
+# # For all, as reported is calculated to Darshan
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     group_by(job_id, rank, `#module`) %>%
+#     summarize(n=n(), 
+#         u_ops = toString(unique(op)),
+#         distinct_operations = paste(u_ops, collapse = ', '),
+#         total_dur = sum(`seg:dur`),
+#         total_bytes = sum(`seg:len`)) %>%
+#     arrange(job_id, total_dur) %>%
+#     select(job_id, `#module`, rank, distinct_operations, total_dur, total_bytes) %>%
+#     tail(6) %>%
+#     as.data.frame() 
+
+# # For all, as reported is calculated to Darshan
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     group_by(job_id, `#module`, rank) %>%
+#     summarize(n=n(), 
+#         u_ops = toString(unique(op)),
+#         distinct_operations = paste(u_ops, collapse = ', '),
+#         total_dur = sum(`seg:dur`),
+#         total_bytes = sum(`seg:len`)) %>%
+#     arrange(job_id, total_dur) %>%
+#     select(job_id, `#module`, rank, distinct_operations, total_dur, total_bytes) %>%
+#     head(6) %>%
+#     as.data.frame() 
+
+# # For all, as reported is calculated to Darshan
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256576") %>%
+#     group_by(job_id, rank, `#module`) %>%
+#     summarize(n=n(), 
+#         u_ops = toString(unique(op)),
+#         distinct_operations = paste(u_ops, collapse = ', '),
+#         total_dur = sum(`seg:dur`),
+#         total_bytes = sum(`seg:len`)) %>%
+#     arrange(job_id, total_dur) %>%
+#     select(job_id, `#module`, rank, distinct_operations, total_dur, total_bytes) %>%
+#     tail(6) %>%
+#     as.data.frame() 
+
+# # For all, as reported is calculated to Darshan
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256576") %>%
+#     group_by(job_id, `#module`, rank) %>%
+#     summarize(n=n(), 
+#         u_ops = toString(unique(op)),
+#         distinct_operations = paste(u_ops, collapse = ', '),
+#         total_dur = sum(`seg:dur`),
+#         total_bytes = sum(`seg:len`)) %>%
+#     arrange(job_id, total_dur) %>%
+#     select(job_id, `#module`, rank, distinct_operations, total_dur, total_bytes) %>%
+#     head(6) %>%
+#     as.data.frame() 
+
+# Cumulative time spent read/writing at the POSIX level
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     group_by(job_id, op, ProducerName) %>%
+#     summarize(sum_duration = sum(`seg:dur`)) %>%
+#     as.data.frame() 
+
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     filter(type == "MET") %>%
+#     group_by(job_id, op) %>%
+#     summarize(sum_duration = sum(`seg:dur`)) %>%
+#     as.data.frame() 
+
+# Duration of the slowest/fastest individual POSIX read/write
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     filter(op == "write") %>%
+#     arrange(-`seg:dur`) %>%
+#     select(op, `seg:dur`)
+
+# # Duration of the slowest/fastest individual POSIX read/write
+# df.hacc %>% 
+#     filter(`#module` == "POSIX") %>%
+#     filter(job_id == "256542") %>%
+#     filter(op != "close") %>%
+#     group_by(rank) %>%
+#     summarize(total_dur = sum(`seg:dur`)) %>%
+#     arrange(total_dur)
+
+# total_STDIO_F_SLOWEST_RANK_TIME:
+# df.hacc %>%
+#     filter(`#module` == "STDIO") %>%
+#     filter(job_id == "256542") %>%
+#     filter(op == "write") %>%
+#     arrange(-`seg:dur`) %>%
+#     select(rank, op, type) %>%
+#     as.data.frame()
+
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(Start = `seg:timestamp` - `seg:dur`) %>%
+#     mutate(End = `seg:timestamp`) %>%
+#     group_by(job_id) %>%
+#     mutate(End = End - min(Start),
+#         Start = Start - min(Start)) %>%
+#     ungroup() %>%
+#     filter(op=="open") %>%
+#     filter(`#module` == "POSIX") %>%
+#     head(1) %>%
+#     as.data.frame()
+
+# Get consecutive POSIX operations (read/write)
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     filter(`#module` == "POSIX") %>%
+#     select(op, rank, `seg:off`, ProducerName) %>%
+#     group_by(gr = cumsum(op != lag(op, 
+#         default = first(op)))) %>%
+#     mutate(count = n()) %>%
+#     as.data.frame()
+
+# Timestamp of the first operation execution
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     filter(`seg:timestamp` != 0) %>%
+#     select(`seg:timestamp`) %>%
+#     head(1)
+
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(Start = `seg:timestamp` - `seg:dur`) %>%
+#     mutate(End = `seg:timestamp`) %>%
+#     mutate(End = End - min(Start),
+#          Start = Start - min(Start)) %>%
+#     mutate(Duration = End - Start) %>%
+#     select(`#module`, op, Start, End, Duration) %>%
+#     filter(`#module` == "POSIX") %>%
+#     arrange(Duration)
+
+################################# START
+# total_POSIX_F_WRITE_START_TIMESTAMP
+# Using their start timestamp as zero
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     mutate(start =  end - `seg:dur`) %>%
+#     select(`#module`, rank, op, job_id, start, `seg:timestamp`) %>%
+#     filter(op == "write") %>%
+#     filter(`#module` == "POSIX") %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+# # total_POSIX_F_READ_START_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     mutate(start =  end - `seg:dur`) %>%
+#     select(`#module`, rank, op, job_id, start, `seg:timestamp`) %>%
+#     filter(op == "read") %>%
+#     filter(`#module` == "POSIX") %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+# # total_POSIX_F_OPEN_START_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     mutate(start =  end - `seg:dur`) %>%
+#     select(`#module`, rank, op, job_id, start, `seg:timestamp`) %>%
+#     filter(op == "open") %>%
+#     filter(`#module` == "POSIX") %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+# # total_POSIX_F_CLOSE_START_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     mutate(start =  end - `seg:dur`) %>%
+#     select(`#module`, rank, op, job_id, start, `seg:timestamp`) %>%
+#     filter(op == "close") %>%
+#     filter(`#module` == "POSIX") %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+# ################################# END
+# # total_POSIX_F_WRITE_END_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     select(`#module`, rank, op, job_id, end, `seg:timestamp`) %>%
+#     filter(op == "write") %>%
+#     filter(`#module` == "POSIX") %>%
+#     arrange(-end) %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+# # total_STDIO_F_READ_START_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     select(`#module`, rank, op, job_id, end, `seg:timestamp`) %>%
+#     filter(op == "read") %>%
+#     arrange(-end) %>%
+#     filter(`#module` == "POSIX") %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+# # total_STDIO_F_OPEN_END_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     filter(`seg:timestamp` != 0) %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     select(`#module`, rank, op, job_id, end, `seg:timestamp`) %>%
+#     filter(op == "open") %>%
+#     filter(`#module` == "POSIX") %>%
+#     arrange(-end) %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+# # total_STDIO_F_CLOSE_END_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     mutate(end = `seg:timestamp` - 1658512320) %>%
+#     select(`#module`, rank, op, job_id, end, `seg:timestamp`) %>%
+#     filter(op == "close") %>%
+#     filter(`#module` == "POSIX") %>%
+#     arrange(-end) %>%
+#     head(1) %>%
+#     as.data.frame() 
+
+    # ggplot(aes(ymin=rank-0.5, ymax=rank+0, xmin=start, xmax=end+0.5)) +
+    # geom_rect(color="black", size=0.03, alpha=1) + my_theme() + 
+    # theme(panel.grid.major.x=element_blank()) +
+    # ggtitle("POSIX write operations for HACC-IO - Lustre 256542") +
+    # scale_x_continuous("Execution time", expand=c(0,0), breaks=seq(0,1000,25)) + 
+    # scale_y_continuous("Ranks", expand=c(0,0)) -> p
+
+    # ggsave(filename=paste("./figs/", "haccio_ganttchart.png", sep=""),
+    # plot = p,
+    # height=4,
+    # width=10)
+
+################################# END
+# total_POSIX_F_WRITE_END_TIMESTAMP
+# df.hacc %>%
+#     filter(job_id == "256542") %>%
+#     filter(`#module` == "POSIX") %>% 
+#     group_by(job_id, `#module`, op) %>%
+#     summarize(total_duration = sum(`seg:dur`)) %>%
+#     as.data.frame() 
+
+################################# total_POSIX_F_MAX_READ_TIME
+df.hacc %>%
+    filter(job_id == "256542") %>%
+    filter(`#module` == "POSIX") %>% 
+    filter(op == "read") %>%
+    arrange(-`seg:dur`) %>%
+    select(`#module`, op, ProducerName, rank, max_byte, rank, type, `seg:len`, `seg:dur`) %>%
+    head(3) %>%
+    as.data.frame() 
+
+df.hacc %>%
+    filter(job_id == "256542") %>%
+    filter(`#module` == "POSIX") %>% 
+    filter(op == "write") %>%
+    select(`#module`, op, ProducerName, rank, max_byte, rank, type, `seg:len`, `seg:dur`) %>%
+    arrange(-`seg:dur`) %>%
+    head(3) %>%
+    as.data.frame() 
